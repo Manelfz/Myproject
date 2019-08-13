fbfef4a73e7672c84c3340f7e2698e453f359129
Manelfz <54027772+Manelfz@users.noreply.github.com>
2019-08-13 23:19:55 +0200

    Add files via upload

---
+++ tests/achatsBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace achatsBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
